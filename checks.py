3409f8ea326ccd658a3d77774b74a711c6410619
snow6oy <snow6oy@gmail.com>
2018-07-12 23:07:17 +0100

    checks microservice has logic

--- checks.py
+++ checks.py
@@ -3,20 +3,36 @@
 checks microservice
 '''
 import bottle
-from bottle import route, run, request
+from bottle import route, run, request, response, abort
 
-if __name__ == '__main__':
-  #cas=CASClient()
-  server=bottle.app()
+class accountChecker():
 
-  @route('/checks', method='post')
-  def new_check():
+  def new_check(self, person):
     ''' 
     check if person is allowed an account
     '''
+    self.check=dict()
+    if person=='camilla':
+      self.check['results']={'person':'camilla', 'pass':True}
+    elif person=='graham':
+      self.check['results']={'person':'graham', 'pass':False}
+
+
+if __name__ == '__main__':
+  ac=accountChecker()
+  server=bottle.app()
+
+  @route('/checks', method='post')
+  def checks():
+    '''
+    location header is set but GET /checks/:person is not implemented
+    '''
     person=request.forms.get('person')
-    check=dict()
-    check['results']={'person':'camilla', 'pass':True}
-    return check
+    ac.new_check(person)
+    if 'results' in ac.check:
+      response.set_header('Location', "/checks/%s" % person)
+    else:
+      abort(404, "No such person %s" % person)
+    return ac.check
 
   run(app=server, host='0.0.0.0', port=5002)
